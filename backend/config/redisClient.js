b7c1706361c9038a865c6688e5c073f78f88981f
AmanBhujel <bhujelaman20@gmail.com>
2023-11-21 11:24:47 +0545

    Commiting redis changes

--- backend/config/redisClient.js
+++ backend/config/redisClient.js
@@ -1,5 +1,7 @@
-const { Redis } = require('ioredis');
 
-const client = new Redis();
+
+const { Redis } = require('ioredis');
+const { REDIS_URL } = process.env;
+const client = new Redis(REDIS_URL);
 
 module.exports = client;
